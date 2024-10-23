8185a715820be8d03f3ac80cbdcd26ac2bd1e45d
Peter Gyorfi <hello@apgyorfi.dev>
2024-10-23 12:28:17 +0200

    pass match validation

--- form-validator/script.js
+++ form-validator/script.js
@@ -19,9 +19,13 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(input) {
+function checkEmail(input) {
   const re = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
-  return re.test(String(input).toLowerCase());
+  if (re.test(input.value)) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 
 // Chech required fields
@@ -35,6 +39,30 @@ function checkRequired(inputArr) {
   });
 }
 
+//Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} most be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} most be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+//Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
 //Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -45,4 +73,8 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
